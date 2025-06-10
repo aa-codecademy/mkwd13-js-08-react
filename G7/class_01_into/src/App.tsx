5e278c073f8e771487cc2bece233e32944a15c65
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-06-10 19:44:57 +0200

    class 1 init

---
+++ G7/class_01_into/src/App.tsx
@@ -0,0 +1,38 @@
+import './App.css';
+import { Fragment } from 'react';
+import StudentDetails from './StudentDetails';
+import AcademyDetails from './AcademyDetails';
+import StyledComponent from './StyledComponent';
+
+function App() {
+	const name = 'John Doe';
+	// option 1: Adding an addition wrapper element
+	return (
+		<div>
+			<h1>React Basics</h1>
+			<StudentDetails name={name} />
+			<AcademyDetails />
+			<StyledComponent />
+		</div>
+	);
+	// option 2: Adding an empty wrapper
+	// return (
+	// 	<>
+	// 		<h1>React Basics</h1>
+	// 		<div className='red-bg'>Test</div>
+	// 	</>
+	// );
+	// option 3: Using a Fragment
+	// return (
+	// 	<Fragment>
+	// 		<h1>React Basics</h1>
+	// 		<div className='red-bg'>Test</div>
+	// 	</Fragment>
+	// );
+	// option 4: Using an array (Try to avoid)
+	// return [<div>item 1</div>, <div>item 2</div>];
+}
+
+// class Student {}
+
+export default App;
---
+++ G7/class_01_into/src/StudentDetails.tsx
@@ -0,0 +1,14 @@
+function StudentDetails(props: { name: string }) {
+	const age = 20;
+
+	return (
+		<div className='red-bg'>
+			<h1>Name: {props.name}</h1>
+			<h2>Location: Skopje, Macedonia</h2>
+			<h3>Age: {age}</h3>
+			<p>John is a student of software engineering</p>
+		</div>
+	);
+}
+
+export default StudentDetails;
