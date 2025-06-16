a8bd99fd4bc8220e245eff910c71fbe411582234
borisovski-borche <75560716+borisovski-borche@users.noreply.github.com>
2025-06-16 20:35:29 +0200

    class-04-02

--- G1/class-04-routing/e-store-app/src/Layout/Header/Header.tsx
+++ G1/class-04-routing/e-store-app/src/Layout/Header/Header.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+import Navbar from "../../Components/Navbar/Navbar";
 import "./Header.css";
 
 function Header() {
   return (
     <header className="Header">
-      <h1>E Store App</h1>
+      <h1>
+        <Link to="/">E Store App</Link>
+      </h1>
+      <Navbar />
     </header>
   );
 }
--- G1/class-04-routing/e-store-app/src/Pages/ProductsPage/ProductsPage.tsx
+++ G1/class-04-routing/e-store-app/src/Pages/ProductsPage/ProductsPage.tsx
@@ -1,10 +1,26 @@
+import ProductCard from "../../Components/ProductCard/ProductCard";
+import type { Product } from "../../models/product.model";
 import "./ProductsPage.css";
 
-function ProductsPage() {
+interface ProductsPageProps {
+  products: Product[];
+  addToCart: (selectedProduct: Product) => void;
+}
+
+function ProductsPage({ products, addToCart }: ProductsPageProps) {
   return (
-    <section className="page">
+    <section className="page ProductsPage">
       <div className="page-heading">
         <h2>Products</h2>
+      </div>
+      <div className="card-container">
+        {products.map(product => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            addToCart={addToCart}
+          />
+        ))}
       </div>
     </section>
   );
